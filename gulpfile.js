99608bae960c35ee527956cbb4bc7ee7c1a9c6a2
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-26 16:51:08 +0300

    added components for gulp rename , added bootstrap library and included it in gulp.js, added task for libraries for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglifyjs');
 //***********
@@ -21,8 +22,22 @@ gulp.task('browser-sync', function() {
 //
 //SASS
 //
+gulp.task('sassLib', function() {
+    return gulp.src([
+            './node_modules/bootstrap/dist/css/bootstrap.min.css'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(rename('styleLib.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./ss'));
+});
 gulp.task('sass', function() {
-    return gulp.src('./src/sass/style.min.scss')
+    return gulp.src([
+            './src/sass/style.min.scss'
+        ])
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
@@ -36,7 +51,10 @@ gulp.task('sass', function() {
 
 gulp.task('compressjS', function() {
     return gulp.src(
-    ['node_modules/angular/angular.min.js','./src/js/*.js'])
+            [
+                'node_modules/angular/angular.min.js', //angular
+                './src/js/*.js',
+            ])
         .pipe(uglify('script.min.js', {
             outSourceMap: true,
             mangle: false
@@ -57,4 +75,4 @@ gulp.task('sass:watch', function() {
     gulp.watch("src/sass/custom/*.scss").on('change', browserSync.reload);
     gulp.watch("src/js/*.js").on('change', browserSync.reload);
 });
-gulp.task('default', ['sass', 'compressjS', 'sass:watch', 'browser-sync']);
+gulp.task('default', ['sass','sassLib', 'compressjS', 'sass:watch', 'browser-sync']);
