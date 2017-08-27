3bb948b9766ea609c171de6832f4f518ae514e5e
Ciprian Turcu <turcuciprian1@gmail.com>
2017-08-27 04:52:29 +0300

    correct routing conditions applied

--- src/js/models/routes.js
+++ src/js/models/routes.js
@@ -1,14 +1,27 @@
-(function () {
-    app.config(function($routeProvider) {
+(function() {
+    app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        var isLogged = function($q, $rootScope, $location, $http) {
+            $location.path('/login');
+            return true;
+        }
         $routeProvider
             .when("/login", {
-                templateUrl: "src/views/login.html"
+                templateUrl: "src/views/login.html",
+                resolve: {
+                    factory: isLogged
+                }
             })
             .when("/dashboard", {
-                templateUrl: "src/views/dashboard.html"
+                templateUrl: "src/views/dashboard.html",
+                resolve: {
+                    factory: isLogged
+                }
             })
             .otherwise({
-                templateUrl: "src/views/login.html"
+                templateUrl: "src/views/login.html",
+                resolve: {
+                    factory: isLogged
+                }
             });
-    });
+    }]);
 })();
